3e803ce3f5f3219b463a8c80e54f81585a43ebf9
MusiroABM <alvarobasaurit@gmail.com>
2021-03-20 17:05:41 +0100

    modificaciones

---
+++ frontend/src/app/modificar-senderista/modificar-senderista.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modificar-senderista',
+  templateUrl: './modificar-senderista.component.html',
+  styleUrls: ['./modificar-senderista.component.css']
+})
+export class ModificarSenderistaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
