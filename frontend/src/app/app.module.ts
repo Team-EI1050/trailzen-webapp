54ea480f0a1291ef71a281158e1ff8980f61b511
iscervera <iscervera.tirado@gmail.com>
2021-03-20 12:55:19 +0100

    Sigue en proceso

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SenderistaComponent } from './senderista/senderista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SenderistaComponent
   ],
   imports: [
     BrowserModule,
