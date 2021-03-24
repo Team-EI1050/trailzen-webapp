0cf3a16298eb444a09cf24bed7c92386de16ea13
iscervera <iscervera.tirado@gmail.com>
2021-03-24 18:16:22 +0100

    Corregido el error de undefined al cargar el senderista

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,6 +7,6 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  id = '605b70ae40993c1bad26f8bf';
+  id = '605b7397b67007250398a46d';
   title = 'TrailZen';
 }
