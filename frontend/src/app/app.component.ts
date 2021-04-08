582762641aea0b5a9410b2e09e02f38836f1b6e3
MusiroABM <alvarobasaurit@gmail.com>
2021-04-08 21:17:48 +0200

    pc

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,6 +7,6 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  id = '6064ba53682e2021c83c4457';
+  id = '606f555ca6c371263457424d';
   title = 'TrailZen';
 }
