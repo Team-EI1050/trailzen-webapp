3e197c83d2594b387fbf59df5bda10707e2b0823
Alvaro <al361914@uji.es>
2021-04-12 14:49:14 +0200

    WIP

--- frontend/src/app/senderista/modal-eliminar-senderista/modal-eliminar-senderista.component.ts
+++ frontend/src/app/senderista/modal-eliminar-senderista/modal-eliminar-senderista.component.ts
@@ -25,6 +25,7 @@ export class ModalEliminarSenderistaComponent implements OnInit {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      console.log(this.closeResult);
     });
   }
 
