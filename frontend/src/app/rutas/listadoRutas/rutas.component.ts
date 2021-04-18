b1f982c14cef586771caefc58cfe14d80dcf325c
iscervera <iscervera.tirado@gmail.com>
2021-04-18 18:56:10 +0200

    Funciona el login pero 200 no es mayor que 0(?)

--- frontend/src/app/rutas/listadoRutas/rutas.component.ts
+++ frontend/src/app/rutas/listadoRutas/rutas.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Ruta } from 'src/app/modelos/ruta';
+import { RutaService } from '../ruta.service';
 
 @Component({
   selector: 'app-rutas',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RutasComponent implements OnInit {
 
-  constructor() { }
+  ruta: Ruta;
+
+  constructor(private activatedRoute: ActivatedRoute, private rutaService: RutaService) { }
 
   ngOnInit() {
+
+    // this.getId = this.activatedRoute.snapshot.paramMap.get('id');
+    // console.log("Id:" + this.getId);
+    this.rutaService.getRuta('6075a29951822b2eb1f8f43c').subscribe(res => {
+      console.log("Res:" + res);
+      this.ruta = res;
+      console.log("senderista:" + this.ruta);
+    })
+
+
   }
 
 }
