194809153deab754d2eb09b2d9f6755ad9211bf7
albertoPcatalan@gmail.com <GloAl_28>
2021-04-18 23:02:05 +0200

    solucionado problema del natbar

--- frontend/src/app/common/login/user.service.ts
+++ frontend/src/app/common/login/user.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { throwError } from 'rxjs';
-import { Iuser } from '../modelos/Iuser';
+import { Iuser } from '../../modelos/Iuser';
 
 @Injectable({
   providedIn: 'root'
--- frontend/src/app/common/navbar/navbar.component.ts
+++ frontend/src/app/common/navbar/navbar.component.ts
@@ -9,12 +9,16 @@ import { Iuser } from 'src/app/modelos/Iuser';
 })
 export class NavbarComponent implements OnInit {
 
+  user: Iuser;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem("USER"));
   }
 
-  obtenerUser(): String{
+  // Returns the id of the current user
+  getUserId(): String {
     let us :Iuser = JSON.parse(localStorage.getItem("USER"));
     return us.nickname;
   }
