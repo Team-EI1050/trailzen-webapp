5a773469823c07dcd091858fc17564cf01ef6354
iscervera <iscervera.tirado@gmail.com>
2021-04-27 18:17:37 +0200

    Arreglado el autoborrado de la sesion

--- frontend/src/app/rutas/detalles-ruta/detalles-ruta.component.ts
+++ frontend/src/app/rutas/detalles-ruta/detalles-ruta.component.ts
@@ -20,9 +20,7 @@ export class DetallesRutaComponent implements OnInit {
 
   ngOnInit(): void {
 
-    console.log(this.user);
-    console.log(JSON.parse(localStorage.getItem("USER")));
-    console.log(this.user)
+    this.user = JSON.parse(localStorage.getItem("USER"));
 
     this.getId = this.activatedRoute.snapshot.paramMap.get('id');
     console.log("Id:" + this.getId);
