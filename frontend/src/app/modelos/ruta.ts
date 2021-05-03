a6f4ecb259f34d006e7a3168a4c936cd4013ea5b
Phywer <al375914@gmail.com>
2021-05-03 15:59:54 +0200

    Actualizado filtro id, falta coordenadas

--- frontend/src/app/modelos/ruta.ts
+++ frontend/src/app/modelos/ruta.ts
@@ -6,7 +6,24 @@ export class Ruta {
   circular: Boolean ;
   aprobada: Boolean ;
   fotos: [ { nombre: String , url: String } ];
-  viable: Boolean 
+  viable: Boolean;
   descripcion: String;
+  creador: String;
   valoraciones: [ {valoracion: Number, nickname: String} ] 
+
+
+  constructor(id: String, nombre: String, distancia:Number, coordenadas: [],
+    circular:Boolean, aprobada: Boolean, viable:Boolean, descripcion:String, creador: String){
+    this._id = id;
+    this.nombre=nombre;
+    this.distancia=distancia;
+    this.coordenadas= coordenadas;
+    this.circular=circular;
+    this. aprobada= aprobada;
+    this.fotos=null;
+    this.viable = viable;
+    this.descripcion = descripcion;
+    this.creador = creador;
+    this.valoraciones=null;
+}
 }
--- frontend/src/app/rutas/proponer-ruta/proponer-ruta.component.ts
+++ frontend/src/app/rutas/proponer-ruta/proponer-ruta.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { UserService } from 'src/app/common/login/user.service';
 import { Iuser } from 'src/app/modelos/Iuser';
+import { Ruta } from 'src/app/modelos/ruta';
 import { SenderistaService } from 'src/app/senderista/senderista.service';
 
 @Component({
@@ -15,20 +17,18 @@ export class ProponerRutaComponent implements OnInit {
   validos:      Boolean;
   data:         String;
 
-  constructor(private userService: UserService, private senderistaService: SenderistaService) { 
+  constructor(private userService: UserService, private senderistaService: SenderistaService, private rutaService,  private router: Router) { 
     this.user = JSON.parse(localStorage.getItem("USER"));
     console.log(this.user);
     this.miFormulario = new FormGroup({ //atributos del formulario para el resgitro del senderista
       nombre:    new FormControl('', Validators.required),
       distancia: new FormControl('', Validators.required),
-      puntoInico:  new FormControl('', Validators.required),
-      puntoFin:  new FormControl('', Validators.required),
+      ruta: new FormControl(Validators.required),
       circular:  new FormControl('', Validators.required),
       aprobada: new FormControl(true),
       viable: new FormControl(true),
       creador: new FormControl(this.user._id),
       descripcion: new FormControl('', Validators.required),
-
     });
     this.validos = true; 
     this.data = "Uno de tus campos es incorrecto";
@@ -38,5 +38,47 @@ export class ProponerRutaComponent implements OnInit {
     this.user = JSON.parse(localStorage.getItem("USER"));
   }
 
+  proponerRuta(){
+      let id=(this.miFormulario.controls['nombre'].value);
+      let nombre=(this.miFormulario.controls['nombre'].value);
+      let distancia=(this.miFormulario.controls['distancia'].value);
+      let ruta=(this.miFormulario.controls['ruta'].value);
+      let aprobada=true;
+      let viable= true;
+      let circular=(this.miFormulario.controls['circular'].value);
+      let descripcion=(this.miFormulario.controls['descripcion'].value);
+      let creador=nombre;
+      
+
+      if (nombre == '' ||  distancia == '' ||  ruta.length < 3 || descripcion == ''){
+        this.validos = false;
+        this.miFormulario.controls['nombre'].setValue("");
+        this.miFormulario.controls['distancia'].setValue("");
+      }else{
+        this.rutaService.getRuta(id).subscribe(res => {
+          if (res==null){ //registra la nueva ruta
+            let nuevaRuta=new Ruta(id,nombre,distancia,ruta,circular,aprobada, viable,descripcion,creador)
+            this.rutaService.addRuta(nuevaRuta).subscribe(nuevo => {
+              if(nuevaRuta==null) {
+                console.log("Problema al crear la ruta");
+              }
+              else{
+                console.log("Ruta registrada");
+                this.router.navigate(['/']);
+                
+              }
+            });
+          }
+          else { //si ya existe ese nickname, muestra un error y pone los campos a null
+            this.validos = false;
+              this.miFormulario.controls['nombre'].setValue("");
+              this.miFormulario.controls['distancia'].setValue("");
+          }
+        });
+      }  
+    
+  }
+
 }
+
 
